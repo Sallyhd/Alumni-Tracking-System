83d71b669cc03224af6bb1800e51d6424194e438
Sally <100796368+Sallyhd@users.noreply.github.com>
2022-08-03 21:36:59 -0500

    Update Student.java

--- src/main/java/miu/edu/AlumniTrackingSystem/entity/Student.java
+++ src/main/java/miu/edu/AlumniTrackingSystem/entity/Student.java
@@ -1,4 +1,22 @@
 package miu.edu.AlumniTrackingSystem.entity;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
 public class Student {
+    @Id
+    @GeneratedValue
+    private  int id;
+
+    private String firstname;
+    private String lastname;
+    private  String email;
+    private String username;
+    private String password;
+    private double gpa;
 }
