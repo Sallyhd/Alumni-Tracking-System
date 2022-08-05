c98dbaafcd1635676ebf8a2445d209c7b6eaac8d
Abenezer G. Alemu <52355638+AbenezerGenanaw@users.noreply.github.com>
2022-08-05 11:52:26 -0500

    Faculty services, repo, controller with get all

--- src/main/java/miu/edu/AlumniTrackingSystem/entity/Faculty.java
+++ src/main/java/miu/edu/AlumniTrackingSystem/entity/Faculty.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Entity
 @Data
 @PrimaryKeyJoinColumn
-public class Faculty extends  User {
+public class Faculty extends User {
 
     @OneToMany(mappedBy = "faculty")
     private List<Comment> comments;
