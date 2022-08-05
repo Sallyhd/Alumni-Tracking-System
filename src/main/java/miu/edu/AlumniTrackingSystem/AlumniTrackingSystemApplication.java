0da9deb3b257934661ca4b3d166144964c103285
Abenezer G. Alemu <52355638+AbenezerGenanaw@users.noreply.github.com>
2022-08-04 22:34:55 -0500

    Add model mapper

--- src/main/java/miu/edu/AlumniTrackingSystem/AlumniTrackingSystemApplication.java
+++ src/main/java/miu/edu/AlumniTrackingSystem/AlumniTrackingSystemApplication.java
@@ -1,7 +1,9 @@
 package miu.edu.AlumniTrackingSystem;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AlumniTrackingSystemApplication {
@@ -10,4 +12,8 @@ public class AlumniTrackingSystemApplication {
 		SpringApplication.run(AlumniTrackingSystemApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper mapper() {
+		return new ModelMapper();
+	}
 }
