15d5391fbc600ca3e15f7a2d35bfa59cb8685be7
moussadavid <106766447+moussadavid@users.noreply.github.com>
2022-08-06 13:05:06 -0500

    DepartmentService

--- src/main/java/miu/edu/AlumniTrackingSystem/service/DepartmentService.java
+++ src/main/java/miu/edu/AlumniTrackingSystem/service/DepartmentService.java
@@ -1,4 +1,10 @@
 package miu.edu.AlumniTrackingSystem.service;
 
+import miu.edu.AlumniTrackingSystem.dto.DepartmentDTO;
+
+import java.util.Optional;
+
 public interface DepartmentService {
+
+    Optional<DepartmentDTO> getById(int id);
 }
--- src/main/java/miu/edu/AlumniTrackingSystem/service/Impl/DepartmentServiceImpl.java
+++ src/main/java/miu/edu/AlumniTrackingSystem/service/Impl/DepartmentServiceImpl.java
@@ -1,8 +1,25 @@
 package miu.edu.AlumniTrackingSystem.service.Impl;
 
+import miu.edu.AlumniTrackingSystem.dto.DepartmentDTO;
+import miu.edu.AlumniTrackingSystem.repository.DepartmentRepository;
 import miu.edu.AlumniTrackingSystem.service.DepartmentService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class DepartmentServiceImpl implements DepartmentService {
+
+    @Autowired
+    private DepartmentRepository departmentRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+    @Override
+    public Optional<DepartmentDTO> getById(int id) {
+        return departmentRepo.findById(id).map(department->modelMapper.map(department,DepartmentDTO.class));
+    }
 }
